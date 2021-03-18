81f660b997ea784f9fc706d0a1e4714d540d79f4
Leo <azat.abdrashitov7@gmail.com>
2021-03-18 12:12:07 +0300

    finish Class

--- task2.py
+++ task2.py
@@ -39,14 +39,13 @@ class Student:
 
 
 
-
-
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = [] #Список доступных курсов на проверку
         self.grade_lecturer=[]
+
 
 #Проверяющие ДЗ
 class Reviewers(Mentor):
@@ -64,6 +63,7 @@ class Reviewers(Mentor):
         else:
             return "Ошибка"
 
+
 #Лекторы, которым студенты ставят оценки
 class Lecturer(Mentor):
     def __str__(self):
@@ -78,40 +78,73 @@ class Lecturer(Mentor):
 
 
 lector1=Lecturer("O","N") #создали лектора
-lector1.addCourses() #добавили ему курс
+lector1.addCourses() #добавили курс лектору 1
+
 lector2=Lecturer("O","M")
-lector2.addCourses()
+lector2.addCourses() #добавили курс лектору 2
 
-print()
-
-pupil1=Student("Azat","Abdrashitov","M") #создали студента
-pupil1.courses_in_progress.append("GIT") #добавили курс студенту
-pupil1.courses_in_progress.append("Python") #добавили курс студенту
+pupil1=Student("Azat","Abdrashitov","M") #создали студента 1
+pupil1.courses_in_progress.append("GIT") #добавили курс студенту 1
+pupil1.courses_in_progress.append("Python") #добавили курс студенту 1
 pupil1.finished_courses.append("English") # добавили завершенный курс
-pupil1.gradeLecturer(lector1) #оценка 1
-pupil1.gradeLecturer(lector1) #оценка 2
-pupil1.gradeLecturer(lector2)
+pupil1.gradeLecturer(lector1) #оценка1 лектору 1 --
+pupil1.gradeLecturer(lector1) #оценка2 лектору 1 -- --=>все от студента1
+pupil1.gradeLecturer(lector2) #оценка1 лекетору2 --
 
-pupil2=Student("Leo","Messi","M")
-pupil2.courses_in_progress.append("GIT") #добавили курс студенту
-pupil2.courses_in_progress.append("Python") #добавили курс студенту
+
+pupil2=Student("Leo","Messi","M") #создали студента 2
+pupil2.courses_in_progress.append("GIT") #добавили курс студенту 2
+pupil2.courses_in_progress.append("Python") #добавили курс студенту 2
 pupil2.finished_courses.append("None") # добавили завершенный курс
+pupil2.gradeLecturer(lector2) #оценка1 лектору2 от студента2
+
+
+homework1=Reviewers("Boris","Trushin") #создали проверяющего 1
+homework1.courses_attached.append("GIT") #добавили курс проверяющему 1
+homework1.courses_attached.append("Python") #добавили курс проверяющему 1
+homework1.rate_hw(pupil1,"GIT",7) #поставили оценку 7 студенту1
+homework1.rate_hw(pupil1,"Python",3) #поставили оценку 3 студенту1
+homework1.rate_hw(pupil1,"Python",2) #поставили оценку 2 студенту1
+homework1.rate_hw(pupil2,"GIT",3) #поставили оценку 3 студенту
+homework1.rate_hw(pupil2,"Python",5)
+
+
+
+def gradesStudents(*args,course=""):
+    countGrades=[]
+    for student in args:
+        if course in student.grades:
+            countGrades+=student.grades[course]
+    print(f"Средняя оценка студентов: {[i.name for i in args]}\
+    по курсу: {course} == {sum(countGrades)/len(countGrades)}")
+
+gradesStudents(pupil1,pupil2,course="Python")
+
+
+def gradesLecturers(*args,course=""):
+    countGrades=[]
+    for lector in args:
+        if course in lector.courses_attached:
+            countGrades+=lector.grade_lecturer
+    print(f"Средняя оценка лекторов: {[i.name for i in args]}\
+    по курсу: {course} == {sum(countGrades)/len(countGrades)}")
+
+gradesLecturers(lector1,lector2,course="GIT")
+
+
+
+print(lector1) #Инфа о лекторе1
+print()
+print(lector2) #Инфа о лекторе2
+print()
+print(homework1) #Инфа о проверяющем1
+print()
+print(pupil1) #Инфа о студенте1
+print()
+print(pupil2) #Инфа о студенте2
 print()
 
-
-homework1=Reviewers("Boris","Trushin")
-homework1.courses_attached.append("GIT")
-homework1.courses_attached.append("Python")
-homework1.rate_hw(pupil1,"GIT",7)
-homework1.rate_hw(pupil1,"Python",3)
-homework1.rate_hw(pupil1,"Python",2)
-
-homework1.rate_hw(pupil2,"GIT",3)
-
-##print(lector1) #Инфа о лекторе
-##print(homework1)
-print(pupil1)
-print(pupil2)
+#Сравнение
 a=pupil2<pupil1
 if a==False:
     print(">")
